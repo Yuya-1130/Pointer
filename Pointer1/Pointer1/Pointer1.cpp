8015058a3a57851284cac811feab854d4f245208
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-23 10:02:52 +0900

    課題１

---
+++ Pointer1/Pointer1/Pointer1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	
+	
+	printf("ポインタ版の値を入れ替えるプログラム\n");
+	void Swap(); {
+		int Point = 1;
+		printf("Point が保持する値は %d です\n", Point);
+
+		int* Pointer = &Point;
+
+		*Pointer = 2;
+		printf("Pointer が保持する値は %d です\n", *Pointer);
+	 
+   }
+	printf("参照版の値を入れ替えるプログラム\n");
+	void Swap2(); {
+		int Count = 3;
+		printf("Count が保持する値は %d です\n", Count);
+
+		int& Counter = Count;
+
+		Counter = 4;
+		printf("Counter が保持する値は %d です\n", Counter);
+	}
+
+}
+
