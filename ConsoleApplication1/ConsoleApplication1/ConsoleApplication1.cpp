afa3f3900c19db0966e70aa2d71eeb055e25d43b
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-23 11:24:55 +0900

    ２限目

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,12 +4,14 @@
 
 int main()
 { 
-	// int 型の変数に10 を代入する
-	int a = 10;
-	printf("a が保持する値は %d です。\n", a);
-
-	// 変数のアドレスを int 型のポインタ変数 pointer に代入する
-	int* pointer = &a;
-	printf("pointer が保持する値は %p です。\n", pointer);
+	int array[4];
+	for (int i = 0; i < 4; ++i) {
+		printf("%p\n", &array[i]);
+	}
+	int* p = array;
+	for (int i = 0; i < 4; ++i) {
+		printf("%p\n", p);
+		p++;
+	}
 }
 
