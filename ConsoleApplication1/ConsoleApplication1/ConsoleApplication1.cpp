c5d1fef08148f55cb57ddfb02752ac5e9936207f
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-23 10:17:09 +0900

    1限目

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{ 
+	// int 型の変数に10 を代入する
+	int a = 10;
+	printf("a が保持する値は %d です。\n", a);
+
+	// 変数のアドレスを int 型のポインタ変数 pointer に代入する
+	int* pointer = &a;
+	printf("pointer が保持する値は %p です。\n", pointer);
+}
+
